87187240d7bf3bd6df4e265ded5a83cd6f63c8c3
Glipotions <hamzakavak1905@gmail.com>
2022-02-07 17:23:27 +0300

    invoice

--- lib/screens/satis_fatura_list/components/body.dart
+++ lib/screens/satis_fatura_list/components/body.dart
@@ -47,43 +47,85 @@ class _BodyState extends State<Body> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: ListView.builder(
-        itemCount: satisFaturaList.length,
-        itemBuilder: (context, index) => Padding(
-          key: UniqueKey(),
-          padding:
-              EdgeInsets.symmetric(vertical: getProportionateScreenWidth(10)),
-          child: ListCard(
-            cart: satisFaturaList[index],
-          ),
-          // child: Dismissible(
-          //   key: Key(satisFaturaList[index].id.toString()),
-          //   direction: DismissDirection.endToStart,
-          //   onDismissed: (direction) {
-          //     setState(() {
-          //       satisFaturaList.removeAt(index);
-          //       (context as Element).reassemble();
-          //     });
-          //   },
-          //   background: Container(
-          //     padding: const EdgeInsets.symmetric(horizontal: 20),
-          //     decoration: BoxDecoration(
-          //       color: const Color(0xFFFFE6E6),
-          //       borderRadius: BorderRadius.circular(15),
-          //     ),
-          //     child: Row(
-          //       children: const [
-          //         Spacer(),
-          //         Icon(Icons.restore_from_trash),
-          //         // SvgPicture.asset("assets/icons/Trash.svg"),
-          //       ],
-          //     ),
-          //   ),
-          //   child: ListCard(
-          //     cart: satisFaturaList[index],
-          //   ),
-          // ),
-        ),
-      ),
+          itemCount: satisFaturaList.length,
+          itemBuilder: (_, index) {
+            return Padding(
+              key: UniqueKey(),
+              padding: EdgeInsets.symmetric(
+                  vertical: getProportionateScreenWidth(10)),
+              child: ListCard(
+                cart: satisFaturaList[index],
+                trailing: PopupMenuButton(
+                  itemBuilder: (context) {
+                    return [
+                      const PopupMenuItem(
+                        value: 'SatisRaporu',
+                        child: Text("Satış Raporu"),
+                      )
+                    ];
+                  },
+                  onSelected: (String value) {},
+                ),
+              ),
+              // child: Dismissible(
+              //   key: Key(satisFaturaList[index].id.toString()),
+              //   direction: DismissDirection.endToStart,
+              //   onDismissed: (direction) {
+              //     setState(() {
+              //       satisFaturaList.removeAt(index);
+              //       (context as Element).reassemble();
+              //     });
+              //   },
+              //   background: Container(
+              //     padding: const EdgeInsets.symmetric(horizontal: 20),
+              //     decoration: BoxDecoration(
+              //       color: const Color(0xFFFFE6E6),
+              //       borderRadius: BorderRadius.circular(15),
+              //     ),
+              //     child: Row(
+              //       children: const [
+              //         Spacer(),
+              //         Icon(Icons.restore_from_trash),
+              //         // SvgPicture.asset("assets/icons/Trash.svg"),
+              //       ],
+              //     ),
+              //   ),
+              //   child: ListCard(
+              //     cart: satisFaturaList[index],
+              //   ),
+              // ),
+            );
+            return ListTile(
+                leading: const CircleAvatar(
+                  child: Text('Deneme'),
+                ),
+                title: Text('Satış Faturası'),
+                trailing: PopupMenuButton(
+                  itemBuilder: (context) {
+                    return [
+                      const PopupMenuItem(
+                        value: 'SatisRaporu',
+                        child: Text("Satış Raporu"),
+                      )
+                    ];
+                  },
+                  onSelected: (String value) {},
+                ));
+          }),
     );
+  }
+
+  void actionPopUpItemSelected(String value, String name) {
+    String message;
+    if (value == 'SatisRaporu') {
+      message = "x";
+      // You can navigate the user to edit page.
+    } else if (value == 'delete') {
+      message = 'You selected delete for $name';
+      // You can delete the item.
+    } else {
+      message = 'Not implemented';
+    }
+    print(message);
   }
 }
